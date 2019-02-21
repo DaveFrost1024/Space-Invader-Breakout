e296f3d748e8e8cef399491bb6782038537cda59
DualFreezor <davidwang5@outlook.com>
2019-02-21 20:50:07 +1100

    Name changes and recompiled game under current directory.

---
+++ build/CMakeFiles/Space-Invader-Breakout.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Space-Invader-Breakout.dir/main.cpp.o"
+  "CMakeFiles/Space-Invader-Breakout.dir/common.cpp.o"
+  "CMakeFiles/Space-Invader-Breakout.dir/LTexture.cpp.o"
+  "CMakeFiles/Space-Invader-Breakout.dir/Paddle.cpp.o"
+  "CMakeFiles/Space-Invader-Breakout.dir/Dot.cpp.o"
+  "CMakeFiles/Space-Invader-Breakout.dir/Obstacle.cpp.o"
+  "CMakeFiles/Space-Invader-Breakout.dir/Projectile.cpp.o"
+  "Space-Invader-Breakout.pdb"
+  "Space-Invader-Breakout"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Space-Invader-Breakout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
